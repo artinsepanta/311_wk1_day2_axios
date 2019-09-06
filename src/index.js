b9130185007983df0d7fd4d065837be02ef424a7
artinsepanta <faribasalehi@hotmail.com>
2019-09-06 12:16:46 -0500

    correctiononFailure

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 // import axios here
  let axios = require("axios");
-// Please note that it is normally not considered best practice to commit 
-// api keys to github as it presents a security risk. It is done here only 
-// for practice purposes as we are sharing the same account
+
 const api_key = 'd771b19ef336ed8381def3a60b574464'
 
 const discoverMovie = () => {
@@ -25,7 +23,7 @@ const getMovieById = (id) => {
    const url = `https://api.themoviedb.org/3/movie/${fakeId}?api_key=${api_key}`
     return axios.get(url)
       .then((response) => {
-       return response;
+       return response.status;
     })
  }
 
